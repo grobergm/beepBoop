b09bd47c826b5012088515313e8f7496679911a5
Matt Groberg <grobergmg@gmail.com>
2019-01-25 08:58:48 -0800

    loop j changes number value to text in toTextArray if cotains 3

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
 
 function roboLooper(number){
   var numberArray=[];
+  var toTextArray=[];
   for (var i=1;i<=number;i++){
     numberArray.push(i.toString())
   }
   numberArray.forEach(function(number){
     for (var j=0;j<number.length;j++){
-      console.log(number[j]);
       if (number[j]==="3"){
         number = "Sorry Dave";
       }
     }
-  });
-  console.log(numberArray);
+    toTextArray.push(number);
+    console.log(toTextArray);
+  })
+
 }
 
-roboLooper(10);
+roboLooper(33);
 
 
 $(document).ready(function(){
