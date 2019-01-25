5d28e76b581dedadfc589b73234ad67cd3547152
Matt Groberg <grobergmg@gmail.com>
2019-01-25 09:13:16 -0800

    use includes logic for other text; toTextArray now containg the correct values

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@
 function roboLooper(number){
   var numberArray=[];
   var toTextArray=[];
-  for (var i=1;i<=number;i++){
+  for (var i=0;i<=number;i++){
     numberArray.push(i.toString())
   }
   numberArray.forEach(function(number){
     if(number.includes("3")){
-      toTextArray.push("Sorry Dave");
+      toTextArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else if(number.includes("2")){
+      toTextArray.push("Boop");
+    } else if(number.includes("1")){
+      toTextArray.push("Beep");
+    } else {
+      toTextArray.push(number);
     }
 
 
