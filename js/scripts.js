f1f9ba66e1c27663132714819e32c275fd220dbb
Matt Groberg <grobergmg@gmail.com>
2019-01-25 08:39:57 -0800

    convert numbers to string, add new array, create loop within a loop to cycle through each character of each entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 
 function roboLooper(number){
+  var numberArray=[];
   for (var i=1;i<=number;i++){
-    console.log(i)
+    numberArray.push(i.toString())
   }
+  numberArray.forEach(function(number){
+    for (var j=0;j<number.length;j++){
+      console.log(number[j]);
+      if (number[j]==="3"){
+        number = "Sorry Dave";
+      }
+    }
+  });
+  console.log(numberArray);
 }
 
 roboLooper(10);
 
 
 $(document).ready(function(){
-  alert("jQuery Connected");
-})
+});
